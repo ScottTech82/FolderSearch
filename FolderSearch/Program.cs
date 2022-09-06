21c03ec1faf135f8ea7a851ec34bd4930b4dd398
Josh Scott <joshscott82@gmail.com>
2022-09-06 16:46:47 -0400

    Add project files.

---
+++ FolderSearch/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Runtime.CompilerServices;
+
+
+DisplayAllFolders(@"C:\repos\");
+
+void DisplayAllFolders(string folder, bool displayFiles = false)
+{
+    string[] folders = Directory.GetDirectories(folder);
+    foreach(string f in folders)
+    {
+        Console.WriteLine(f);
+        if (displayFiles)
+            DisplayAllFilesInFolder(f);
+        DisplayAllFolders(f);
+    }
+}
+
+void DisplayAllFilesInFolder(string folder)
+{
+    string[] files = Directory.GetFiles(folder);
+    foreach(string file in files)
+    {
+        Console.WriteLine(file);
+    }
+}
+
+
+/*
+GetSubFolders();
+ 
+void GetSubFolders()
+{
+    string foldername = @"C:\repos";
+    string[] FolderArray = Directory.GetDirectories(foldername);
+    foreach (string folder in FolderArray)
+    {
+    Console.WriteLine(folder);
+        string[] SubFolder = Directory.GetDirectories(folder);
+        foreach (string subfolder in SubFolder)
+        {
+            Console.WriteLine(subfolder);
+            string[] Files = Directory.GetFiles(subfolder);
+            foreach (string file in Files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+
+    
+}
+*/
+
+
+//string foldername = @"C:\repos";
+//
+//
+//string[] folders = Directory.GetDirectories(foldername);
+//foreach (string folder in folders)
+//{
+//    Console.WriteLine(folder);
+//}
+//
+//string subfolder = $"C:\\{folder}";
+//string[] subfolders = Directory.GetDirectories(subfolder);
+//
+//
+//string FolderFunc(string folder)
+//{
+//    return folder + FolderFunc(folder + subfolder);
+//}
+//Console.WriteLine(subfolder);
